00fbe2efbe9c9dacb2e245be7663c89f19598071
rahul188 <rahulkuchhadia@gmail.com>
2020-08-22 17:31:00 +0530

    Updates

---
+++ precache-manifest.3d579d291a765df4cdbee874eec68d67.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d8039b1e16d0a0a5c7fe8b681437a9b",
+    "url": "/netflix-react-app/index.html"
+  },
+  {
+    "revision": "2d7075abf1449dc36c0d",
+    "url": "/netflix-react-app/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "505b922d5ae76297685d",
+    "url": "/netflix-react-app/static/css/main.2dea798d.chunk.css"
+  },
+  {
+    "revision": "2d7075abf1449dc36c0d",
+    "url": "/netflix-react-app/static/js/2.c2afe958.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/netflix-react-app/static/js/2.c2afe958.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "505b922d5ae76297685d",
+    "url": "/netflix-react-app/static/js/main.57f67834.chunk.js"
+  },
+  {
+    "revision": "04a925593ae9ed8a525b",
+    "url": "/netflix-react-app/static/js/runtime-main.c05402a0.js"
+  }
+]);
